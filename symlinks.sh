1f88229cd8fe5c738ce4ba81114440cd3722b68f
Linus Denning <Linus@Denning.nl>
2024-08-14 22:46:37 +0100

    added script to create symlinks

---
+++ symlinks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+function link {
+	local TARGET="${HOME}/${2}${1}"
+
+	if [ -f $TARGET ] || [ -d $TARGET ]; then
+		echo "$TARGET already exists."
+	else
+		ln -s "${SCRIPT_DIR}/${1}" "${HOME}/${2}"
+		echo "$TARGET link created"
+	fi
+}
+
+link .gitconfig
+link .themes/
+link nvim .config/
