acaaa948378addb42b0d712629018ac7b353eadf
Linus Denning <Linus@Denning.nl>
2024-09-18 09:11:24 +0200

    added comments

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -1,11 +1,12 @@
-return {
+return { -- load with lazy
 	"morhetz/gruvbox",
-	lazy = false,
+	lazy = false, -- ensures vim commands are excecuted after lazy
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 
 	config = function()
 		vim.cmd.colorscheme("gruvbox")
 
+		-- highlught group commands
 		vim.cmd.hi("TreesitterContext guibg=bg")
 	end,
 }
