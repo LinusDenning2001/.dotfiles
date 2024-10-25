f10a77ad7a9f65ef7e39a846e61b48bdac8a826b
Linus Denning <Linus@Denning.nl>
2024-10-25 14:29:16 +0200

    added function to set pyright src path to directory opened by vim (instead of $HOME)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -156,7 +156,11 @@ require("lazy").setup({
 			local servers = {
 				-- clangd = {},
 				-- gopls = {},
-				pyright = {},
+				pyright = {
+					root_dir = function()
+						return vim.fn.getcwd()
+					end,
+				},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
